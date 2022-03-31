c3d63a6cd3fb6b20cee1afaea38feeba1872ebdd
Миша <vsisoev@ukr.net>
2022-03-31 20:33:07 +0300

    Create Context and prepare db

---
+++ Infrastructure/CmsShoppingCartContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CmsShoopingCart.Infrastructure
+{
+    public class CmsShoppingCartContext : DbContext
+    {
+        public CmsShoppingCartContext(DbContextOptions<CmsShoppingCartContext> options)
+        {
+
+        }
+    }
+}
